88bea162a163a6f5c10e5530c3c51a5b079b7caa
Benchis <benediktas.dap@gmail.com>
2023-10-28 12:56:32 +0300

    final

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,19 +6,27 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public GameObject jumpscare;
-    public GameObject finnish;
+    AudioSource source;
+    
     public string sceneName;
-
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     private void Update()
     {
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
-        GetComponent<Rigidbody2D>().MovePosition(mousePos);
+        if (jumpscare.GetComponent<SpriteRenderer>().enabled == false)
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            GetComponent<Rigidbody2D>().MovePosition(mousePos);
+        }
+        
     }
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Finish"))
+        if (collision.gameObject.name == "Finish")
         {
             
             if (sceneName != null)
@@ -30,6 +38,8 @@ public class Player : MonoBehaviour
         {
             jumpscare.GetComponent<SpriteRenderer>().enabled = true;
             Invoke("BackToMenu", 3f);
+            source.Play();
+            
         }
         
 
